292d499a3f9dfb45081b5a8097b4bf99cc26563a
Pietro de Deus Medeiros <spogmedeiros@gmail.com>
2021-07-03 18:32:56 -0300

    add midleware

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ server.set('view engine', 'ejs')
 server.use(express.static("public"))
 server.set('views', path.join(__dirname, 'views'))
 
+server.use(express.urlencoded({extended: true}))
+
 server.use(route)
 
 server.listen(3000, () => console.log("YES! Server is running!"))
