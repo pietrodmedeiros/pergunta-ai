76f7b3a2e7198fa4fd088164b9328d4002ccdf79
Pietro de Deus Medeiros <spogmedeiros@gmail.com>
2021-07-03 19:12:20 -0300

    add -include at index to render container

--- src/route.js
+++ src/route.js
@@ -2,9 +2,9 @@ const express = require('express')
 const route = express.Router()
 const QuestionController = require('./controllers/QuestionController')
 
-route.get('/', (req, res) => res.render('index'))
+route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
 route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
 
 route.post('/room/:room/:question/:action', QuestionController.index)
 
