ab9ed0f428ca6f8db2c39b3956fb361c883c8284
Pietro de Deus Medeiros <spogmedeiros@gmail.com>
2021-07-04 15:48:39 -0300

    now app creates room

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -44,7 +44,7 @@ function handleClick(event, check = true){
     const roomId = document.querySelector("#room-id").dataset.id
     const questionId = event.target.dataset.id
 
-    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML = `${text} esta pergunta`
     modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
