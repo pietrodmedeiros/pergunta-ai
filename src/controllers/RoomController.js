170a83159f10a2105bc3b784f8520d8db20aafd0
Pietro de Deus Medeiros <spogmedeiros@gmail.com>
2021-07-05 14:43:05 -0300

    creating new room with pass

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,8 +1,26 @@
-module.exports = {
-    create(req, res){
-        let roomId = 123456
+const Database = require("../db/config")
 
+module.exports = {
+    async create(req, res){
+        const db = await Database()
+        const pass = req.body.password
+        let roomId = ""
+
+        for(var i = 0; i < 6; i++){
+            roomId += Math.floor(Math.random() * 10).toString()
+        }
+
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        ) VALUES (
+            ${parseInt(roomId)},
+            ${pass}
+        )`)
+
+        await db.close()
         createInfoBD = 
         res.redirect(`/room/${roomId}`)
+        
     }
 }
